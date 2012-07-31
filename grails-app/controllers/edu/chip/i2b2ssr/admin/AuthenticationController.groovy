a17a8ad0639a385fa693c1332c1aa791ffed61a4
Dave Ortiz <daveortiz@gmail.com>
2012-07-31 06:42:12 -0400

    Scaffolding of LDAP auth service

---
+++ grails-app/controllers/edu/chip/i2b2ssr/admin/AuthenticationController.groovy
@@ -0,0 +1,14 @@
+package edu.chip.i2b2ssr.admin
+
+class AuthenticationController {
+
+    def index() { }
+
+    def login = {
+
+        String username = params.username
+        String password = params.password
+
+
+    }
+}
