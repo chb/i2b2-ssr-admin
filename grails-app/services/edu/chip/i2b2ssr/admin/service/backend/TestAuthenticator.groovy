ce84c250429631db1291745ec8ba3c899bcfa06c
Dave Ortiz <daveortiz@gmail.com>
2012-08-01 20:12:24 -0400

    New Features and Tests:
    Tests for AuthenticationService
    New background QuerySweeper Task

--- grails-app/services/edu/chip/i2b2ssr/admin/service/backend/TestAuthenticator.groovy
+++
@@ -1,7 +0,0 @@
-package edu.chip.i2b2ssr.admin.service.backend
-
-class TestAuthenticator implements IAuthenticator{
-  boolean authenticate(String username, String password) {
-    return true
-  }
-}
