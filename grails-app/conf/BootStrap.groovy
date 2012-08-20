b53dfa80b83cda2bc713c3ea6ae8eae52318db91
Dave Ortiz <daveortiz@gmail.com>
2012-08-20 11:38:01 -0400

    Preferences fixup -punting on configuring LDAP from UI

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import edu.chip.i2b2ssr.admin.data.Preference
+
 class BootStrap {
 
     def init = { servletContext ->
+      if(Preference.all.size() == 0){
+        //if there's no preferences in the DB add some new ones
+        new Preference().save(failOnError: true)
+      }
     }
     def destroy = {
     }
--- grails-app/domain/edu/chip/i2b2ssr/admin/data/Preference.groovy
+++ grails-app/domain/edu/chip/i2b2ssr/admin/data/Preference.groovy
@@ -1,9 +1,6 @@
 package edu.chip.i2b2ssr.admin.data
 
 class Preference {
-    String ldapAddress = "ldap://example"
-    String ldapBaseDN  = "cn=users"
-    String i2b2OntCell = "http://ont.i2b2.com"
-    String shrineCell = "http://carra-core-produciton:8080"
-
+    String i2b2OntCell = "http://carra-i2b2-production:8080/ont/"
+    String shrineCell = "http://carra-core:8080"
 }
