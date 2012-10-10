7767eae64b95b25b237a1f666f95fe54f817aa15
Dave Ortiz <daveortiz@gmail.com>
2012-10-10 13:12:50 -0400

    more fixes found in test env

--- grails-app/controllers/edu/chip/i2b2ssr/admin/PermissionController.groovy
+++ grails-app/controllers/edu/chip/i2b2ssr/admin/PermissionController.groovy
@@ -10,64 +10,65 @@ import edu.chip.i2b2ssr.admin.data.User
 class PermissionController {
 
 
-    def create = {
-        List<Study> studies = new ArrayList<Study>()
-        User u = User.get(params.userId)
-        studies.addAll(studiesWithNoPermissions(params.userId))
-        [user: u, study: Study.get(params.studyid), availableStudies: studies]
+  def create = {
+    List<Study> studies = new ArrayList<Study>()
+    User u = User.get(params.userId)
+    studies.addAll(studiesWithNoPermissions(params.userId))
+    [user: u, study: Study.get(params.studyid), availableStudies: studies]
+  }
+
+  def edit = {
+    Permission p = Permission.get(params.permissionId)
+    [user: p.user, study: p.study, permission: p]
+  }
+
+
+  private Set<Study> studiesWithNoPermissions(String userId) {
+    User u = User.get(userId)
+    Set<Study> studies = new HashSet<Study>()
+    studies.addAll(Study.all)
+
+    if(u) {
+      Collection<Study> addedStudies = User.get(userId)?.permissions*.study
+      studies.removeAll(addedStudies)
+    }
+    return studies
+
+
+  }
+
+  def save = {
+    User u = User.get(params.userId)
+    Study s = Study.get(params.studyId)
+
+
+    Permission p = Permission.get(params.permissionId)
+
+    if(p == null) {
+      p = new Permission(alllowPdo: params.allowPdo,
+              allowSiteIdentify: params.allowSiteIdentify)
+
+      u.addToPermissions(p)
+      s.addToPermission(p)
+    }
+    else {
+
+      p.setProperties(allowPdo: params.allowPdo,
+              allowSiteIdentify: params.allowSiteIdentify)
+
     }
 
-    def edit = {
-        Permission p = Permission.get(params.permissionId)
-        [user: p.user, study: p.study, permission: p]
+
+    if(u != null && s && !p.hasErrors()) {
+      flash["message"] = "Saved Permission"
+      p.save(failOnError: true)
+      redirect(controller: "User", action: "list")
     }
-
-
-    private Set<Study> studiesWithNoPermissions(String userId) {
-        User u = User.get(userId)
-        Set<Study> studies = new HashSet<Study>()
-        studies.addAll(Study.all)
-
-        if (u) {
-            Collection<Study> addedStudies = User.get(userId)?.permissions*.study
-            studies.removeAll(addedStudies)
-        }
-        return studies
-
-
+    else {
+      flash["message"] = "Error saving permission, please try again"
+      redirect(controller: "Permission", action: "create", params: params)
     }
-
-    def save = {
-        User u = User.get(params.userId)
-        Study s = Study.get(params.studyId)
-
-
-        Permission p = Permission.get(params.permissionId)
-
-        if (p == null) {
-            p = new Permission(alllowPdo: params.allowPdo,
-                    allowSiteIdentify: params.allowSiteIdentify)
-
-            u.addToPermissions(p)
-            s.addToPermission(p)
-        }
-        else {
-            p.setProperties(alllowPdo: params.allowPdo,
-                    allowSiteIdentify: params.allowSiteIdentify)
-
-        }
-
-
-        if (u != null && s && !p.hasErrors()) {
-            flash["message"] = "Saved Permission"
-            p.save(failOnError: true)
-            redirect(controller: "User", action: "list")
-        }
-        else {
-            flash["message"] = "Error saving permission, please try again"
-            redirect(controller: "Permission", action: "create", params: params)
-        }
-    }
+  }
 
 
 }
