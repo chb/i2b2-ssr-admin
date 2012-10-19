3506936df76c96424df8479a6b91407e70011e98
Dave Ortiz <daveortiz@gmail.com>
2012-10-19 13:12:37 -0400

    statuses for machine

--- grails-app/domain/edu/chip/i2b2ssr/admin/data/Machine.groovy
+++ grails-app/domain/edu/chip/i2b2ssr/admin/data/Machine.groovy
@@ -2,27 +2,34 @@ package edu.chip.i2b2ssr.admin.data
 
 class Machine {
 
-    static def SHRINE_OK = "Good"
-    static def MACHINE_AVAILABLE = "Available"
-    static def MACHINE_BAD = "Bad"
+  static def REACHABLE = "Reachable"
+  static def UNREACHABLE = "Unreachable"
 
-    String name
-    String certificate
-    String realName
-    Integer lastPatientCount = 0
-    Long lastResponseTimeInMillis = -1
-    URL url
-    String status = MACHINE_BAD
-    static hasMany = [studies: Study]
-    static belongsTo = Study
-    static constraints = {
-        certificate nullable: true
-        name unique: true
-        lastPatientCount nullable: true
-        lastResponseTimeInMillis nullable: true
-        status(nullable: true, is: ([SHRINE_OK,
-                MACHINE_AVAILABLE,
-                MACHINE_BAD]))
+  String name
+  String certificate
+  String realName
+
+
+  URL url
+  String endpointStatus = UNREACHABLE
+  SortedSet<Status> statuses
+  static hasMany = [studies: Study, statuses: Status]
+  static belongsTo = Study
+  static constraints = {
+    certificate nullable: true
+    name unique: true
+    endpointStatus(nullable: true, is: ([REACHABLE,
+            UNREACHABLE]))
+  }
+
+  def Status latestStatus() {
+    if(statuses?.size() > 0) {
+      statuses.last()
     }
+    else {
+      null
+    }
+  }
+
 
 }
--- test/unit/edu/chip/i2b2ssr/admin/data/MachineTests.groovy
+++ test/unit/edu/chip/i2b2ssr/admin/data/MachineTests.groovy
@@ -1,19 +1,36 @@
 package edu.chip.i2b2ssr.admin.data
 
 import grails.test.mixin.*
+import org.junit.Test
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Machine)
+@Mock([Study, Machine])
 class MachineTests {
 
-    void testCreateBasicMachine() {
-        Machine p = new Machine(name: "Dave", realName: "test", url: new URL("http://test.com"))
-        assertEquals(p.name, "Dave")
-        assertEquals("http://test.com", p.url.toString())
-        p.save(failOnError: true)
-        assertEquals(1, Machine.all.size())
-        assertEquals("Dave", Machine.get(1).name)
-    }
+  @Test
+  void testCreateBasicMachine() {
+    Calendar c1 = Calendar.getInstance()
+    Status s1 = new Status(timeStamp: c1.getTime(), numberOfPatients: 1, responseTimeInMillis: 10)
+    c1.add(Calendar.HOUR, 1)
+    Status s2 = new Status(timeStamp: c1.getTime(), numberOfPatients: 2, responseTimeInMillis: 20)
+    c1.add(Calendar.HOUR, 1)
+    Status s3 = new Status(timeStamp: c1.getTime(), numberOfPatients: 3, responseTimeInMillis: 30)
+
+    Machine p = new Machine(name: "Dave", realName: "test", url: new URL("http://test.com"))
+    p.addToStatuses(s1)
+    p.addToStatuses(s2)
+    p.addToStatuses(s3)
+    assertEquals(p.name, "Dave")
+    assertEquals("http://test.com", p.url.toString())
+    p.save(failOnError: true)
+    assertEquals(1, Machine.all.size())
+    assertEquals("Dave", Machine.get(1).name)
+    assertEquals(p.statuses.size(), 3)
+
+    assertEquals(p.latestStatus(), s3)
+
+  }
 }
