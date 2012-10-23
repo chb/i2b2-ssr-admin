af207bf39c788b8d415905f8b21333289bf1be7d
Dave Ortiz <daveortiz@gmail.com>
2012-10-23 15:40:10 -0400

    update

--- grails-app/controllers/edu/chip/i2b2ssr/admin/PreferenceController.groovy
+++ grails-app/controllers/edu/chip/i2b2ssr/admin/PreferenceController.groovy
@@ -20,8 +20,8 @@ class PreferenceController {
   def update = {
     try {
       Preference p = Preference.all.first()
-      p.shrineCell = trimSlash(p.shrineCell)
-      p.i2b2OntCell = trimSlash(p.i2b2OntCell)
+      p.shrineCell = trimSlash(params.shrineCell)
+      p.i2b2OntCell = trimSlash(params.i2b2OntCell)
       if(params.heartBeatStudy && params.heartBeatStudy != "null") {
         p.heartBeatStudyId
       }
@@ -44,7 +44,7 @@ class PreferenceController {
   //removes trailing slash at end of URL so we can make
   private String trimSlash(String url) {
     if(url.endsWith("/")) {
-      return url.substring(0, url.size() - 2)
+      return url.substring(0, url.size() - 1)
     }
     else {
       return url
