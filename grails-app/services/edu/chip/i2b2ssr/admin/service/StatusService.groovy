0b1d4128be8bb5e6f27292b692a9466369ef1351
Dave Ortiz <daveortiz@gmail.com>
2012-11-06 09:16:38 -0500

    round 1 of Status refactor

--- grails-app/services/edu/chip/i2b2ssr/admin/service/StatusService.groovy
+++ grails-app/services/edu/chip/i2b2ssr/admin/service/StatusService.groovy
@@ -5,10 +5,49 @@ import edu.chip.i2b2ssr.admin.data.QuerySession
 import edu.chip.i2b2ssr.admin.data.User
 import edu.chip.i2b2ssr.admin.data.Preference
 import org.apache.jasper.tagplugins.jstl.core.Url
+import net.shrine.service.JerseyShrineClient
+import net.shrine.protocol.AuthenticationInfo
+import net.shrine.protocol.Credential
+import edu.harvard.i2b2.crc.datavo.setfinder.query.ResultOutputOptionType
+import net.shrine.protocol.ResultOutputType
+import net.shrine.protocol.RunQueryResponse
+import edu.chip.i2b2ssr.admin.data.Status
 
 class StatusService {
   static transactional = true
 
+  private String heartbeatQueryPanel = """
+        <ns4:query_definition xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                        xmlns:ns8="http://www.i2b2.org/xsd/hive/msg/result/1.1/"
+                        xmlns:ns6="http://www.i2b2.org/xsd/cell/crc/psm/analysisdefinition/1.1/"
+                        xmlns:ns5="http://www.i2b2.org/xsd/cell/crc/psm/querydefinition/1.1/"
+                        xmlns:imsg="http://www.i2b2.org/xsd/hive/msg/1.1/"
+                        xmlns:ns3="http://www.i2b2.org/xsd/hive/plugin/"
+                        xmlns:ns4="http://www.i2b2.org/xsd/cell/crc/psm/1.1/"
+                        xmlns:ns2="http://www.i2b2.org/xsd/hive/pdo/1.1/">
+                  <query_name>CARRAnet </query_name>
+                  <query_description>Status Query</query_description>
+                  <query_timing>ANY</query_timing>
+                  <specificity_scale>0</specificity_scale>
+                  <panel name="Panel_31">
+                      <panel_number>1</panel_number>
+                      <panel_accuracy_scale>0</panel_accuracy_scale>
+                      <invert>0</invert>
+                      <total_item_occurrences>1</total_item_occurrences>
+                      <item>
+                          <hlevel>1</hlevel>
+                          <item_name>root</item_name>
+                          <item_key>\\</item_key>
+                          <dim_tablename>concept_dimension</dim_tablename>
+                          <dim_columnname>concept_path</dim_columnname>
+                          <dim_dimcode>\\</dim_dimcode>
+                          <dim_columndatatype>T</dim_columndatatype>
+                          <facttablecolumn>concept_cd</facttablecolumn>
+                          <item_is_synonym>false</item_is_synonym>
+                      </item>
+                  </panel>
+              </ns4:query_definition>
+  """
 
   def checkEndpointStatus() {
     log.info("Running status check on cluster")
@@ -47,11 +86,20 @@ class StatusService {
     def tempSession = new QuerySession(user: u)
     tempSession.save(failOnError: true, flush: true)
     Preference p = Preference.first()
-    if(p.heartBeatStudy?.studyName) {
-      def url = new URL(Preference.first().shrineCell +
-              "/statusCheck?username=\"${u.userName}\"&password=\"${tempSession.sessionId}\"&project=\"${p.heartBeatStudy.studyName}\"")
-      String response = url.getContent().toString()
+
+    AuthenticationInfo auth = new AuthenticationInfo(u.userName, new Credential(tempSession.sessionId, true))
+
+    for(Machine m : Machine.all) {
+      JerseyShrineClient client = new JerseyShrineClient(p.shrineCell, "machine-${m.name}", auth, true)
+      long start = System.currentTimeMillis();
+      RunQueryResponse r = client.runQuery("", ResultOutputType.PATIENT_COUNT_XML, heartbeatQueryPanel)
+      int numberOfPatients = r.RR
+      long end = System.currentTimeMillis()
+
+      m.addToStatuses(new Status(numberOfPatients: 0, responseTimeInMillis: (end - start)))
+      m.save(failOnError: true)
     }
+
     tempSession.delete(flush: true)
   }
 
