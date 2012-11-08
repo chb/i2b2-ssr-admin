5b69abc2cc8e2f78d99d8ab55cde964074b826d8
Dave Ortiz <daveortiz@gmail.com>
2012-11-08 13:53:52 -0500

    fix typo

--- grails-app/services/edu/chip/i2b2ssr/admin/service/backend/LDAPAuthenticator.groovy
+++ grails-app/services/edu/chip/i2b2ssr/admin/service/backend/LDAPAuthenticator.groovy
@@ -20,61 +20,61 @@ import com.unboundid.ldap.sdk.*
  */
 class LDAPAuthenticator implements IAuthenticator {
 
-  private String baseDn;
-  private String userIdentifier
-  private LDAPConnectionPool pool
+    private String baseDn;
+    private String userIdentifier
+    private LDAPConnectionPool pool
 
-  private static final log = LogFactory.getLog(this)
+    private static final log = LogFactory.getLog(this)
 
-  def LDAPAuthenticator(String ldapAddress, int ldapPort, String ldapBindDn,
-          String ldapBindPassword, String baseDn, String userIdentifier) throws LDAPException {
-    this.baseDn = baseDn
-    this.userIdentifier = userIdentifier
-    LDAPConnection con = new LDAPConnection(ldapAddress, ldapPort, ldapBindDn, ldapBindPassword)
-    pool = new LDAPConnectionPool(con, 10, 20);
-  }
-
-
-  def LDAPAuthenticator(String ldapAddress, int ldapPort, String baseDn, String userIdentifier) throws LDAPException {
-    this.baseDn = baseDn;
-    this.userIdentifier = userIdentifier;
-    LDAPConnection con = new LDAPConnection(ldapAddress, ldapPort);
-    pool = new LDAPConnectionPool(con, 10, 20);
-  }
-
-
-  def boolean authenticate(String username, String password) {
-    def con = pool.getConnection()
-
-    try {
-
-      SearchResult sr = con.search(baseDn, SearchScope.SUB, (userIdentifier + "=" + username));
-      if(sr.getSearchEntries().size() < 1) {
-        throw new PermissionException("No such user");
-      }
-
-      if(sr.getEntryCount() > 0) {
-        String dn = sr.getSearchEntries().get(0).getDN();
-        con.bind()
-        pool.releaseDefunctConnection(con)
-        return true
-
-      }
-      else {
-        return false
-      }
-
-    } catch(LDAPException e) {
-      log.error("Recieved LDAP exception: " + e.message);
-      return false
-    }
-    finally {
-      if(con) {
-        pool.releaseConnection(con)
-      }
-
+    def LDAPAuthenticator(String ldapAddress, int ldapPort, String ldapBindDn,
+                          String ldapBindPassword, String baseDn, String userIdentifier) throws LDAPException {
+        this.baseDn = baseDn
+        this.userIdentifier = userIdentifier
+        LDAPConnection con = new LDAPConnection(ldapAddress, ldapPort, ldapBindDn, ldapBindPassword)
+        pool = new LDAPConnectionPool(con, 10, 20);
     }
 
 
-  }
+    def LDAPAuthenticator(String ldapAddress, int ldapPort, String baseDn, String userIdentifier) throws LDAPException {
+        this.baseDn = baseDn;
+        this.userIdentifier = userIdentifier;
+        LDAPConnection con = new LDAPConnection(ldapAddress, ldapPort);
+        pool = new LDAPConnectionPool(con, 10, 20);
+    }
+
+
+    def boolean authenticate(String username, String password) {
+        def con = pool.getConnection()
+
+        try {
+
+            SearchResult sr = con.search(baseDn, SearchScope.SUB, (userIdentifier + "=" + username));
+            if (sr.getSearchEntries().size() < 1) {
+                throw new PermissionException("No such user");
+            }
+
+            if (sr.getEntryCount() > 0) {
+                String dn = sr.getSearchEntries().get(0).getDN();
+                con.bind(dn, password)
+                pool.releaseDefunctConnection(con)
+                return true
+
+            }
+            else {
+                return false
+            }
+
+        } catch (LDAPException e) {
+            log.error("Recieved LDAP exception: " + e.message);
+            return false
+        }
+        finally {
+            if (con) {
+                pool.releaseConnection(con)
+            }
+
+        }
+
+
+    }
 }
