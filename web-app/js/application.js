847cdd262a1b6b60941dcd0c7743b04b5d129ab0
Dave Ortiz <daveortiz@gmail.com>
2012-11-15 14:31:43 -0500

    Fixes related to heartbeat: - New dependency on custom SHRINE, won't build for anyone else currently until my changes hit SHRINE-1.13 - Added heartbeat - Status screen shows historical data chart for patient counts and machine response times

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,3 +1,5 @@
+
+
 if (typeof jQuery !== 'undefined') {
     (function ($) {
         $('#spinner').ajaxStart(function () {
@@ -5,7 +7,6 @@ if (typeof jQuery !== 'undefined') {
         }).ajaxStop(function () {
                 $(this).fadeOut();
             });
-
 
 
         $('.deleteuser').click(function (eventObject) {
@@ -30,6 +31,73 @@ if (typeof jQuery !== 'undefined') {
 
         });
 
+        $(document).ready(function() {
+
+            var g = $('.graph').first()
+
+            for (var i=0; i< 60; i++) {
+                $('#graphs').append(g.clone());
+            }
+
+            var options = {
+                xaxis : {
+                    // extend graph to fit the last point
+                    max : 31
+                },
+                grid : {
+                    show : false
+                }
+            };
+
+            // main series
+            var series = [{
+                data : [],
+                color : '#000000',
+                lines : {
+                    lineWidth : 0.8
+                },
+                shadowSize : 0
+            }];
+
+            // color the last point red.
+            series.push({
+                data : [],
+                points : {
+                    show : true,
+                    radius : 1,
+                    fillColor : '#ff0000'
+                },
+                color : '#ff0000'
+            });
+
+            // draw the sparkline
+            $('.graph').each(function(i){
+                var data = $(this).data("values");
+                var newData = [];
+                $.each(data, function(index, dataItem){
+                  newData.push([index, dataItem]);
+                });
+                series[0].data = newData;
+                var endPoint = newData[newData.length -1];
+                series[1].data = [endPoint];
+                options.xaxis.max = newData.length + 1;
+                plot = $.plot($(this).find('.chart'), series, options);
+                var o = plot.pointOffset({x: endPoint[0], y: endPoint[1]});
+                $('<div class="data-point-label">' + endPoint[1] + '</div>').css( {
+                    position: 'absolute',
+                    left: o.left + 5,
+                    top: o.top - 7,
+                    display: 'none'
+                }).appendTo(plot.getPlaceholder()).show();
+                var label = $(this).find('.label');
+                //label.text("Machine " + i+1)
+//                if (endPoint[1] > 75) {
+//                    label.addClass("error");
+//                }
+            });
+
+        });
+
 
     })(jQuery);
 
@@ -37,47 +105,21 @@ if (typeof jQuery !== 'undefined') {
 }
 
 function deleteRow(id, row, message) {
-    var r=confirm("Are you sure?")
-    if (r==true)
-      {
-          $.ajax({
-                  type:"DELETE",
-                  url:"delete/" + id
-              }
+    var r = confirm("Are you sure?")
+    if (r == true) {
+        $.ajax({
+                type:"DELETE",
+                url:"delete/" + id
+            }
 
-          ).done(function () {
-                  row.slideUp();
-                  $(".message").text(Message)
-              });
-      }
+        ).done(function () {
+                row.slideUp();
+                $(".message").text(Message)
+            });
+    }
 }
 
-//show the modal overlay and popup window
-function showPopUpMessage(msg) {
-    overlayElement = document.createElement("div");
-    overlayElement.className = 'modalOverlay';
-    modalWindowElement = document.createElement("div");
-    modalWindowElement.className = 'modalWindow';
-    modalWindowElement.innerHTML = msg;
-    modalWindowElement.style.left = (window.innerWidth - 200) / 2 + "px";
-    document.body.appendChild(overlayElement);
-    document.body.appendChild(modalWindowElement);
-    setTimeout(function () {
-        modalWindowElement.style.opacity = 1;
-        overlayElement.style.opacity = 0.4;
-        overlayElement.addEventListener("click", hidePopUpMessage, false);
-    }, 300);
-}
-//hide the modal overlay and popup window
-function hidePopUpMessage() {
-    modalWindowElement.style.opacity = 0;
-    overlayElement.style.opacity = 0;
-    overlayElement.removeEventListener("click", hidePopUpMessage, false);
-    setTimeout(function () {
-        document.body.removeChild(overlayElement);
-        document.body.removeChild(modalWindowElement);
-    }, 400);
-}
+
 
 
 
