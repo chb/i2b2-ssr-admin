0e7c344d26e78d652c4b773bc36c4ca9f3f1d47a
Dave Ortiz <daveortiz@gmail.com>
2012-12-18 14:18:32 -0500

    push in new resources

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,30 @@
+import edu.chip.i2b2ssr.admin.service.AuthenticationService
+import edu.chip.i2b2ssr.admin.service.backend.IAuthenticator
+import grails.util.Environment
+import edu.chip.i2b2ssr.admin.service.backend.LDAPAuthenticator
+
+// Place your Spring DSL code here
+beans = {
+  switch(Environment.current) {
+    case Environment.DEVELOPMENT:
+      authService(AuthenticationService) {
+        authenticator = {String userName, String password -> true} as IAuthenticator
+      }
+      break
+    case Environment.TEST:
+      authService(AuthenticationService) {
+        authenticator = {String userName, String password -> true} as IAuthenticator
+        grailsApplication = grailsApplication
+      }
+      break
+    case Environment.PRODUCTION:
+      authService(AuthenticationService) {
+        authenticator = new LDAPAuthenticator("carra-nexus", 389, "ou=people,dc=carranet,dc=org", "uid")
+
+        grailsApplication = grailsApplication
+        }
+      break
+
+  }
+
+}
