9d5ec0638c7af4f0f56497b97dc5b021712f5d73
Dave Ortiz <daveortiz@gmail.com>
2013-04-22 09:42:45 -0400

    bad commit, extra characters

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,7 +37,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.20'
-    }                                                                 rm -
+    }
 
     plugins {
         runtime ":hibernate:$grailsVersion"
