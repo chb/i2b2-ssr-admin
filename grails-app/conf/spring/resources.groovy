3daabf9c4b264f0fcc6ada33594abdb2425ceaae
Dave Ortiz <daveortiz@gmail.com>
2013-08-27 13:54:28 -0400

    fix resources.groovy

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -5,26 +5,35 @@ import edu.chip.i2b2ssr.admin.service.backend.LDAPAuthenticator
 
 // Place your Spring DSL code here
 beans = {
-  switch(Environment.current) {
-    case Environment.DEVELOPMENT:
-      authService(AuthenticationService) {
-        authenticator = {String userName, String password -> true} as IAuthenticator
-      }
-      break
-    case Environment.TEST:
-      authService(AuthenticationService) {
-        authenticator = {String userName, String password -> true} as IAuthenticator
-        grailsApplication = grailsApplication
-      }
-      break
-    case Environment.PRODUCTION:
-      authService(AuthenticationService) {
-        authenticator = new LDAPAuthenticator("carra-nexus", 389, "ou=people,dc=carranet,dc=org", "uid")
+    switch (Environment.current) {
+        case Environment.DEVELOPMENT:
+            authService(AuthenticationService) {
+                authenticator = { String userName, String password -> true } as IAuthenticator
 
-        grailsApplication = grailsApplication
-        }
-      break
 
-  }
+            }
+            break
+        case Environment.TEST:
+            authService(AuthenticationService) {
+                authenticator = { String userName, String password -> true } as IAuthenticator
+                grailsApplication = grailsApplication
+            }
+            break
+        case Environment.PRODUCTION:
+            authService(AuthenticationService) {
+
+                //use this to test the test LDAP on carra-nexus
+                //authenticator = new LDAPAuthenticator("carra-nexus", 389, "ou=people,dc=carranet,dc=org", "uid")
+
+
+                authenticator = new LDAPAuthenticator(grailsApplication.config.ldap.host,
+                                                    new Integer(grailsApplication.config.ldap.port),
+                        grailsApplication.config.ldap.bindDn, grailsApplication.config.ldap.password,
+                        grailsApplication.config.ldap.basedn, grailsApplication.config.ldap.useridentifier)
+                grailsApplication = grailsApplication
+            }
+            break
+
+    }
 
 }
